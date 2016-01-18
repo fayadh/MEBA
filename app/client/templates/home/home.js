3a70056a651ea17d281d8f4012bca02b306f27ef
fayadh <fayadh.almosawi@gmail.com>
2016-01-18 13:41:49 -0500

    auto background disabled

--- app/client/templates/home/home.js
+++ app/client/templates/home/home.js
@@ -22,18 +22,18 @@ Template.Home.onRendered(function () {
 	$('.nav-link').mouseenter(function() { $(this).css('color', 'silver') })
 	$('.nav-link').mouseout(function() { $(this).css('color', 'white') })
 	
-	$(document).ready(function(){
-		var backgroundImages=["background_0.png","background_1.png","background_2.png","background_3.png", "background_4.png"];
-
-		var i = Math.floor(Math.random() * backgroundImages.length);
-		var backgroundImage = backgroundImages[i];
-
-		$('.master-yield').css("background-image","url(/background/"+backgroundImage+")");
-		
-		// //Set height for MEBA introductory message.
-// 		var required_height = $(window).height() - $('nav').height() //in px.
-// 		$('.inner-cont').css('height', required_height+'px')
-	});
+// 	$(document).ready(function(){
+// 		var backgroundImages=["background_0.png","background_1.png","background_2.png","background_3.png", "background_4.png"];
+//
+// 		var i = Math.floor(Math.random() * backgroundImages.length);
+// 		var backgroundImage = backgroundImages[i];
+//
+// 		$('.master-yield').css("background-image","url(/background/"+backgroundImage+")");
+//
+// 		// //Set height for MEBA introductory message.
+// // 		var required_height = $(window).height() - $('nav').height() //in px.
+// // 		$('.inner-cont').css('height', required_height+'px')
+// 	});
 });
 
 Template.Home.onDestroyed(function () {
